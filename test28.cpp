5b2aa837de56ce85a41e97d553a35d610c9176ed
hoholee12 <hoholee12@naver.com>
2015-06-23 22:26:45 +0900

    stuff

---
+++ test28.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//namespace gradespace{
+	enum grade{
+		a,
+		b,
+		c,
+		d,
+		e,
+		f
+	};
+//}
+
+//using gradespace::grade;
+int printgrade(grade *test){
+	switch(*test /*just 'test' is an address to *test.*/){
+		case a:
+			printf("a");
+			break;
+		case b:
+			printf("b");
+			break;
+		case c:
+			printf("c");
+			break;
+		case d:
+			printf("d");
+			break;
+		case e:
+			printf("e");
+			break;
+		case f:
+			printf("f");
+			break;
+		default:
+			printf("what?");
+			return(1);
+	}
+	return(0);
+}
+
+int main(int argc, char **argv){
+	if(!argv[1]) argv[1]=static_cast<char *>("1337");
+	grade test=static_cast<grade>(atoi(argv[1]));
+	int result=printgrade(&test);
+	if(result) printf("\n\nfail!");
+	return(result);
+}
