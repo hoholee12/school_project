f06b5768d0e126e0b978eec4f3c0b9efc5868868
hoholee12 <hoholee12@naver.com>
2015-06-24 00:36:53 +0900

    stuff

--- test28.cpp
+++ test28.cpp
@@ -1,10 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h> //sleep
+#include"test3.h"
 
+#include"test3.h" //guarded.
 /*class does not need a wrapper
 while enum does...*/
+//somethings wrong with enum class.
 
+//namespace=enum=class=struct=union all similar syntax
 namespace gradespace{
 	namespace grade_wrapper{
 		enum grade{
@@ -15,41 +19,42 @@ namespace gradespace{
 			e,
 			f
 		};
-	}
+	};
 	class hello{
 	public:
 		hello(){printf("hello!\n");}
 	
 	};
-}
+};
 namespace stuff{
-using namespace gradespace::grade_wrapper;
-int printgrade(grade *test){
-	switch(*test /*just 'test' is an address to *test.*/){
-		case a:
-			printf("a");
-			break;
-		case b:
-			printf("b");
-			break;
-		case c:
-			printf("c");
-			break;
-		case d:
-			printf("d");
-			break;
-		case e:
-			printf("e");
-			break;
-		case f:
-			printf("f");
-			break;
-		default:
-			printf("what?");
-			return(1);
+	using namespace gradespace::grade_wrapper;
+	int printgrade(grade *test){
+		switch(*test /*just 'test' is an address to *test.*/){
+			case a:
+				printf("a");
+				break;
+			case b:
+				printf("b");
+				break;
+			case c:
+				printf("c");
+				break;
+			case d:
+				printf("d");
+				break;
+			case e:
+				printf("e");
+				break;
+			case f:
+				printf("f");
+				break;
+			default:
+				printf("what?");
+				return(1);
+		}
+		return(0);
 	}
-	return(0);
-}}
+};
 
 int main(int argc, char **argv){
 	{using gradespace::hello;
