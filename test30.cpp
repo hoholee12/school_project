4ecb6e005d7864656909cfa806104b75ff873281
hoholee12 <hoholee12@naver.com>
2015-06-30 21:57:51 +0900

    trap every signal your keyboard can produce

---
+++ test30.cpp
@@ -0,0 +1,32 @@
+#include<cstdlib>
+#include<cstdio>
+#include<signal.h>
+#include<cstring>
+#include"tools.h"
+#pragma GCC diagnostic ignored "-Wwrite-strings"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wparentheses"
+//common ascii stuff
+//0~9 => 48~57
+//a~z => 97~122
+//A~Z => 65~90
+
+static int num1=0, nums=0;
+
+void handler(int){
+	printf("fuck you.\n");
+	//printf("%d %d\n", ::num1, ::nums);
+	//exit(0);
+}
+
+static const int zero=0;
+int main(int argc, char **argv, char **envp){
+	int ch;
+	signal(SIGINT, handler); //^C keyboard interrupt
+	signal(SIGTSTP, handler); //^Z tty stop
+	while((ch=getchar())){
+		if((ch>='A')&&(ch<='Z')) ::num1++;
+		else if((ch>='a')&&(ch<='z')) ::nums++;
+	}
+	return 0;
+}
