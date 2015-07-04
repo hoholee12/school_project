65c28ed51072e4d5006b9da93e9230540e4d8495
hoholee12 <hoholee12@naver.com>
2015-07-05 00:20:12 +0900

    stack vs heap

--- test32.cpp
+++ test32.cpp
@@ -12,10 +12,12 @@
 //A~Z => 65~90
 
 int main(){
-	char a[]="fuck off"; //spawn an array
-	char *ptr=&a[0]; //pass the first address to the ptr
+	//unless malloc or new, everything else is on stack.
+
+	char a[]="fuck off"; //spawn an array								//this is on stack
+	char *ptr=&a[0]; //pass the first address to the ptr 				//stack
 	//the above code is basically the same as the under code.
-	char *f="motherfucker"; //truncated
+	char *f="motherfucker"; //truncated 								//this is also on stack
 	
 	printf("%s %s\n", ptr, ptf); //printf's %s needs an ADDRESS of the first character of an array.
 	
