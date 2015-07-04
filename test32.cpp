921e984565e67df566bb5e19373bc74dd1e88d05
hoholee12 <hoholee12@naver.com>
2015-07-05 00:31:34 +0900

    its not a constant. just static.

--- test32.cpp
+++ test32.cpp
@@ -13,11 +13,17 @@
 
 int main(){
 	//unless malloc or new, everything else is on stack.
+	//by that standard, everything here is on stack.
 
-	char a[]="fuck off"; //spawn an array								//this is on stack
-	char *ptr=&a[0]; //pass the first address to the ptr 				//stack
+	char a[]="fuck off"; //spawn an array		//right operand is basically a static char arr[9]="fuck off"; pre-defined even before this. also, its NOT constant.
+	//a[4]=''; empty character constant is not allowed!!
+	a[6]='a';
+	a[7]='a';
+	char *ptr=&a[0]; //pass the first address to the ptr
+	ptr[0]='s';
+	ptr[5]='d';
 	//the above code is basically the same as the under code.
-	char *f="motherfucker"; //truncated 								//this is also on stack
+	char *ptf="motherfucker"; //truncated
 	
 	printf("%s %s\n", ptr, ptf); //printf's %s needs an ADDRESS of the first character of an array.
 	
