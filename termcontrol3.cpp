c81994cd50aec1394d4dd4375be786d961ef2ee4
hoholee12 <hoholee12@naver.com>
2015-09-03 22:36:52 -0700

    termcontrol3

---
+++ termcontrol3.cpp
@@ -0,0 +1,24 @@
+#include<ncurses.h>
+#include<string.h>
+
+//rows==lines==y==row
+//columns==x==col
+
+int main(){
+	char mesg[]="just a string";
+	int row, col;
+
+	initscr(); //enable stdscr mode ie clear everything
+
+	getmaxyx(stdscr, row, col); //get the number of rows and columns
+	mvprintw(row/2, (col-strlen(mesg))/2, "%s", mesg);
+
+	mvprintw(row-2, 0, "this screen has %d rows and %d cols\n", row, col);
+
+
+	refresh();
+	getch();
+	endwin(); //close stdscr mode bullshit happen on terminal if not called!
+
+	return 0;
+}
