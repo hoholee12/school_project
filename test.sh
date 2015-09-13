31bd3dafb61b239d60d456aecc3d7a502bb53c1b
yop yop yop <hoholee12@naver.com>
2015-09-13 17:29:31 +0900

    not mine

--- test.sh
+++ test.sh
@@ -37,7 +37,29 @@ source standard.sh --source --bbpass --supass
 #MORE STUFF: http://www.braun-home.net/michael/mbedit/info/misc/VT100_commands.htm
 
 titlemate Hello, World!
-
+while read -rsn1 ui; do
+    case "$ui" in
+    $'\x1b')    # Handle ESC sequence.
+        # Flush read. We account for sequences for Fx keys as
+        # well. 6 should suffice far more then enough.
+        read -rsn1 -t 0.1 tmp
+        if [[ "$tmp" == "[" ]]; then
+            read -rsn1 -t 0.1 tmp
+            case "$tmp" in
+            "A") printf "Up\n";;
+            "B") printf "Down\n";;
+            "C") printf "Right\n";;
+            "D") printf "Left\n";;
+            esac
+        fi
+        # Flush "stdin" with 0.1  sec timeout.
+        read -rsn5 -t 0.1
+        ;;
+    # Other one byte (char) cases. Here only quit.
+    q) break;;
+    esac
+done
+echo
 
 echo -e "playstation motherfucker\e7\e[18Dfuck\e8boy"
 debug_shell
