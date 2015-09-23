2a2e2d5df499d5d22e975082f775c2c9adea13b3
yop yop yop <hoholee12@naver.com>
2015-09-23 22:22:05 +0900

    some bad code lurking inside

--- homework.c
+++ homework.c
@@ -7,10 +7,14 @@
 #include<windows.h> //Sleep()
 #endif
 
+
+
 //inputstate notes:
 //user== 1,2
 //error== -1
 //quit== -2
+
+
 
 void printarr(int **arr, int row, int col, int user){ //print 2d array
 #ifndef _WIN32
@@ -26,17 +30,47 @@ void printarr(int **arr, int row, int col, int user){ //print 2d array
 			else if (arr[i][j] == 1) printf("O\t"); //player1==O
 			else if (arr[i][j] == 2) printf("X\t"); //player2==X
 		}
-		if (i == 1) switch(user){
+		if (i == 1) switch (user){
 		case -1: printf("\t\tyou can't go there!"); break;
 		default: printf("\t\tplayer%d's turn!", user); break;
 		}
 		printf("\n");
 
 	}
-	
+
 
 }
+/*printarr counts generationally
+**printarr_alt counts by row*col */
 
+void printarr_alt(int **arr, int row, int col, int user){ //print 2d array
+#ifndef _WIN32
+	printf("\x1b[2J");
+#else
+	system("cls");
+#endif
+	int count = 0;
+	printf("\t");
+	for (int i = 1; i <= row; i++) printf("%d\t", i);
+	printf("\n");
+	for (int i = 0; i<row; i++){
+		printf("%d\t", i+1);
+		for (int j = 0; j<col; j++){
+			count++;
+			if (arr[i][j] == 0) printf(".\t");
+			else if (arr[i][j] == 1) printf("O\t"); //player1==O
+			else if (arr[i][j] == 2) printf("X\t"); //player2==X
+		}
+		if (i == 1) switch (user){
+		case -1: printf("\t\tyou can't go there!"); break;
+		default: printf("\t\tplayer%d's turn!", user); break;
+		}
+		printf("\n");
+
+	}
+
+
+}
 
 int advinput(){
 	fflush(stdin); //flush any remaining cr
@@ -44,7 +78,62 @@ int advinput(){
 	char *input = (char *)calloc(1, sizeof(char));
 	scanf("%s", input); //scanf address to string goes like this...
 	if (input[0] == 'q') return 'q';
-	return atoi(input);
+	int var1 = atoi(input);
+	int var2 = 1;
+	for (int i = 0; input[i]; i++){
+		if (input[i] == 'x'){
+			var2 = atoi(&input[i + 1]);
+			break;
+		}
+	}
+	return var1*var2;
+}
+
+typedef struct input_alt{
+	int val1;
+	int val2;
+} input_alt;
+
+input_alt *advinput_alt(){
+	fflush(stdin); //flush any remaining cr
+	input_alt *test = (input_alt *)calloc(1, sizeof(input_alt));
+	char *input = (char *)calloc(1, sizeof(char));
+	scanf("%s", input); //scanf address to string goes like this...
+	if (input[0] == 'q'){
+		test->var1 = 'q';
+		return test;
+	}
+	test->var1 = atoi(input);
+	test->var2 = 1;
+	for (int i = 0; input[i]; i++){
+		if (input[i] == 'x'){
+			test->var2=atoi(&input[i + 1]);
+			break;
+		}
+	}
+	return test;
+}
+
+int userinput_alt(int **arr, int row, int col, int user){
+	int result_r = row, result_c = col, max = row*col, min = 1;
+	input_alt *pass;
+	printf("player%d>>", user);
+	pass = advinput_alt();
+	//printf("%c %d", input, input);
+#ifndef _WIN32
+	sleep(1); //1sec
+#else
+	//Sleep(1000); //1sec
+#endif
+	if (pass->var1 == 'q') return -2;
+	//input -= 48; //ascii 1 is 49 //we dont need this anymore
+	if (pass->var1>max || pass->var1<min) return -1;
+	if (pass->var2>max || pass->var2<min) return -1;
+
+	//printf("%dx%d", result_r, result_c);
+	if (arr[pass->var1][pass->var2] != NULL) return -1; //problem
+	arr[pass->var1][pass->var2] = user;
+	return 0;
 }
 
 int userinput(int **arr, int row, int col, int user){
@@ -92,6 +181,7 @@ void printstatus(int **arr, int row, int col, int inputstate){
 }
 
 int main(int argc, char *argv[]){
+
 	int row=3, col=3;
 	if (argc == 2){
 		row = atoi(argv[1]);
@@ -108,10 +198,10 @@ int main(int argc, char *argv[]){
 	for (;; switchuser++){ //mainloop
 
 		user = switchuser % 2 + 1;
-		printarr(arr, row, col, user);
+		printarr_alt(arr, row, col, user);
 		for (; (inputstate = userinput(arr, row, col, user))!=0;){
 			switch (inputstate){
-			case -1: printarr(arr, row, col, inputstate); break;
+			case -1: printarr_alt(arr, row, col, inputstate); break;
 			case -2: printstatus(arr, row, col, inputstate); break;
 			default:;
 			}
