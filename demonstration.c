bfaceff3b46197a8c94a1a2d8c866a359fb3c372
yop yop yop <hoholee12@naver.com>
2015-10-13 12:45:56 +0900

    homework_unix.c demonstration

---
+++ demonstration.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int **allocarr(int **arr, int row, int col){
+	int i;
+	arr=(int **)calloc(row, sizeof(int));
+	malloc(row*sizeof(int)); //hack
+	for(i=0; i<row; i++){
+		arr[i]=(int *)calloc(col, sizeof(int));
+
+
+	}
+
+	return arr;
+}
+
+void freearr(int **arr, int row, int col){
+	int i;
+	for(i=0; i<row; i++) free(arr[i]);
+	free(arr);
+
+
+}
+
+void printarr(int **arr, int row, int col){
+	int i, j;
+	for(i=0; i<row; i++){
+		for(j=0;j<col;j++){
+			printf("\t%d", arr[i][j]);
+		}
+		printf("\n\n");
+	}
+
+
+}
+
+
+
+int main(){
+	int **arr=NULL;
+	arr=allocarr(arr, 3, 3);
+	arr[2][2]=8;
+	printarr(arr, 3, 3);
+	freearr(arr,3, 3);
+	return 0;
+}
