2ef09e2d313bc891c35e95167cadeafe97e3b38a
yop yop yop <hoholee12@naver.com>
2015-10-28 11:23:35 +0900

    doesnt work in vs.

--- test82.c
+++ test82.c
@@ -3,23 +3,20 @@
 #include<stdlib.h>
 
 
-int main(){
-	void *arr;
+int main() {
+	int *arr;
+
+	arr = malloc(10); /*10bytes allocated*/
+
+	arr[0] = 123; /*4bytes*/
+	arr[1] = 456; /*8bytes*/
+	arr[2] = 789; /*12bytes == 2bytes corrupted*/
 	
-	arr=malloc(10);
-	
-	((int *)arr)[0]=123;
-	((int *)arr)[1]=456;
-	((int *)arr)[2]=789;
-	((int *)arr)[3]=101112;
-	((int *)arr)[4]=131415;
-	
-	((int *)arr)[5]=161718;
-	printf("%d %d %d %d %d %d\n", ((int *)arr)[0], ((int *)arr)[1], ((int *)arr)[2], ((int *)arr)[3], ((int *)arr)[4], ((int *)arr)[5]);
+	printf("%d %d %d\n", arr[0], arr[1], arr[2]);
 	free(arr); /*what?!*/
-	
-	
-	
+
+
+
 	return 0;
-	
+
 }
